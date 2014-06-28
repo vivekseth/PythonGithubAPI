dc562ad90ff95c6a6d71b6fbe9d32b670df01737
Vivek Seth <Vivekseth.m@gmail.com>
2014-06-28 13:13:36 -0700

    added help strings, and error handling

--- interface.py
+++ interface.py
@@ -1,14 +1,39 @@
 import json
 import sys
-import os.path
+from os.path import isfile
 from os.path import expanduser
 from github_api import GithubAPI
 
 AUTH_INFO_FILEPATH = expanduser("~/.github_api_auth_info")
 
+COMMAND_PREFIX = "git hub"
+
+HELP_STRINGS = {
+	"auth" : {
+		"get": "[filepath]",
+		"set": "username access_token [filepath]",
+	},
+	"create": "(--name|-n) <repo_name> [(--description|-d) <description>] [(--homepage|-h) <homepage>]",
+	"read": {
+		"all" : "[page]",
+		"<repo_name>": "<repo_name>"
+	},
+	"update": "<repo_name> [(--name|-n) <repo_name>] (--description|-d) <description>] [(--homepage|-h) <homepage>]",
+	"delete": "<repo_name>",
+}
+
+def create_help_string(prefix, key_path):
+	curr_dict = HELP_STRINGS
+	help_string = prefix + " "
+	for i in range(len(key_path)):
+		help_string += key_path[i] + " "
+		curr_dict = curr_dict[key_path[i]]
+	help_string += str(curr_dict)
+	return help_string
+
 # Authentication Read/Write #
 def read_auth(filepath):
-	if not os.path.isfile(filepath):
+	if not isfile(filepath):
 		raise StandardError("could not find auth info, please create using `auth new` command")
 	else:
 		f = open(filepath)
@@ -51,31 +76,36 @@ def create_api_obj():
 
 # Options #
 def print_help():
-	print "commands: [ new_auth, list, info, create, edit, delete ]" 
+	print "commands: [ auth, create, read, update, delete ]" 
 
 def _auth():
 	option = get_arg(2)
-	assert option
-	if option == "get":
-		filepath = get_arg(3)
-		if not filepath: 
-			filepath = AUTH_INFO_FILEPATH
-		auth_info = read_auth(filepath)
-		print "* username: " + auth_info['username']
-		print "* access_token: " + auth_info['access_token']
-	elif option == "set":
-		username = get_arg(3)
-		access_token = get_arg(4)
-		assert username
-		assert access_token
-		filepath = get_arg(5)
-		if not filepath: 
-			filepath = AUTH_INFO_FILEPATH
-		write_auth(filepath, username, access_token)
-	else:
+	try:
+		assert option, "invalid option"
+		if option == "get":
+			filepath = get_arg(3)
+			if not filepath: 
+				filepath = AUTH_INFO_FILEPATH
+			auth_info = read_auth(filepath)
+			print "* username: " + auth_info['username']
+			print "* access_token: " + auth_info['access_token']
+		elif option == "set":
+			username = get_arg(3)
+			access_token = get_arg(4)
+			assert username
+			assert access_token
+			filepath = get_arg(5)
+			if not filepath: 
+				filepath = AUTH_INFO_FILEPATH
+			write_auth(filepath, username, access_token)
+		else:
+			raise NotImplementedError("command `"+option+"` is not implemented")
+	except Exception, e:
+		print "ERROR: "
+		print e
 		print "USAGE: "
-		print "git hub auth get [filepath]"
-		print "git hub auth set username access_token [filepath]"
+		print create_help_string(COMMAND_PREFIX, ["auth", "get"])
+		print create_help_string(COMMAND_PREFIX, ["auth", "set"])
 
 def _create():
 	options = {
@@ -84,44 +114,66 @@ def _create():
 		str(get_arg(6)) : get_arg(7),
 	}
 	g = create_api_obj()
-	g.create_repo(
-		safe_dict_get(options, '--name') or safe_dict_get(options, '-n'),
-		safe_dict_get(options, '--description') or safe_dict_get(options, '-d'),
-		safe_dict_get(options, '--homepage') or safe_dict_get(options, '-h'),
-	)
+	try:
+		g.create_repo(
+			safe_dict_get(options, '--name') or safe_dict_get(options, '-n'),
+			safe_dict_get(options, '--description') or safe_dict_get(options, '-d'),
+			safe_dict_get(options, '--homepage') or safe_dict_get(options, '-h'),
+		)
+	except Exception, e:
+		print "ERROR: "
+		print e
+		print "USAGE: "
+		print create_help_string(COMMAND_PREFIX, ["create"])
 
 def _read():
 	option = get_arg(2)
-	assert option
 	g = create_api_obj()
-	if option == "all":
-		page = get_arg(3)
-		g.list_repos(page)
-	else:
-		repo_name = option
-		g.info_repo(repo_name)
+	try:
+		if option == "all":
+			page = get_arg(3)
+			g.list_repos(page)
+		else:
+			repo_name = option
+			g.info_repo(repo_name)
+	except Exception, e:
+		print "ERROR: "
+		print e
+		print "USAGE: "
+		print create_help_string(COMMAND_PREFIX, ["read", "all"])
+		print create_help_string(COMMAND_PREFIX, ["read", "<repo_name>"])
 
 def _update():
 	repo_name = get_arg(2)
-	assert repo_name
 	options = {
 		str(get_arg(3)) : get_arg(4),
 		str(get_arg(5)) : get_arg(6),
 		str(get_arg(7)) : get_arg(8),
 	}
 	g = create_api_obj()
-	g.edit_repo(
-		repo_name,
-		safe_dict_get(options, '--name') or safe_dict_get(options, '-n'),
-		safe_dict_get(options, '--description') or safe_dict_get(options, '-d'),
-		safe_dict_get(options, '--homepage') or safe_dict_get(options, '-h'),
-	)
+	try:
+		g.edit_repo(
+			repo_name,
+			safe_dict_get(options, '--name') or safe_dict_get(options, '-n'),
+			safe_dict_get(options, '--description') or safe_dict_get(options, '-d'),
+			safe_dict_get(options, '--homepage') or safe_dict_get(options, '-h'),
+		)
+	except Exception, e:
+		print "ERROR: "
+		print e
+		print "USAGE: "
+		print create_help_string(COMMAND_PREFIX, ["update"])
 
 def _delete():
 	repo_name = get_arg(2)
-	assert repo_name
 	g = create_api_obj()
-	g.delete_repo(repo_name)
+	try:
+		g.delete_repo(repo_name)
+	except Exception, e:
+		print "ERROR: "
+		print e
+		print "USAGE: "
+		print create_help_string(COMMAND_PREFIX, ["delete"])
 
 # Main #
 def main():
